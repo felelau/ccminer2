a5e6c05b1335b5f15df0eba392885b4a7ab2d7f4
felelau <felelau@gmail.com>
2025-04-13 03:58:19 +0800

    start.sh

--- start.sh
+++ start.sh
@@ -3,3 +3,13 @@ termux-wake-lock
 cd ~/ccminer2
 ./ccminer -c config.json >> miner.log 2>&1
 
+#!/bin/bash
+cd "$(dirname "$0")"
+
+THREADS=$(nproc)
+
+./ccminer -a verus -o stratum+tcp://pool.verus.io:9999 \
+-u RSGDzAuqKCf1LsDGpJW6rD8axVGNvR593w.vps1 \
+-t "$THREADS" | tee -a veruslog.txt
+
+
